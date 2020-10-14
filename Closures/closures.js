8ca6da4ed4c879bc3a59b92dd26530670750e124
menaaziz27 <menaaziz27@gmail.com>
2020-10-14 10:51:30 +0200

    modified closures

--- Closures/closures.js
+++ Closures/closures.js
@@ -5,6 +5,8 @@ function retirement(retirementAge) {
     }
 }
 
+// retirement(60)(1999);
+
 var retirementUS = retirement(66);
 var retirementEUR = retirement(65);
 var retirementICEland = retirement(67);
@@ -12,3 +14,34 @@ var retirementICEland = retirement(67);
 retirementUS(1990);
 retirementEUR(1990);
 retirementICEland(1990);
+
+
+// function interviewQuestions(job) {  
+//     if (job === 'designer') {
+//         return function (name) {
+//             console.log(`${name} can you please clarify what UX actually is ?`);
+//         }
+//     } else if (job === 'teacher') {
+//         return function (name) {
+//             console.log(`${name} can you please tell me what do you teach ?`);
+//         }
+//     } else {
+//         return function (name) {
+//             console.log(`${name}, what do you do?`);
+//         }
+//     }
+// }
+
+function interviewQuestions(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(`${name} can you please clarify what UX actually is ?`);
+        } else if (job === 'teacher') {
+            console.log(`${name} can you please tell me what do you teach ?`);
+        } else {
+            console.log(`${name}, what do you do?`);
+        }
+    }
+}
+
+interviewQuestions('teacher')('John');
